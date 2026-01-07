f84cd001a4f754f504fe8c51ba3907f3c88c6968
Bernst4315 <bernst4315@gmail.com>
2026-01-06 18:11:15 -0700

    basic server set up

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
+import "dotenv/config"
 import express from "express"
 
+const flashcard = {
+    front: "hello",
+    back: "hola"
+}
 
-const PORT = 8000
+const app = express()
 
-const app = express();
+app.get("/", (req, res) => {
+    res.json(flashcard)
+})
 
-app.listen(PORT, () => {
-    console.log(`running on port ${PORT}`)
+
+app.listen(process.env.PORT, () => {
+    console.log(`running on port ${process.env.PORT}`)
 })
